308bb477aa86dadc872950b484f96e24403735d5
yumengkang <yumengkang@126.com>
2019-04-08 18:42:08 +0800

    常用钩子总结

--- webpack-plugin/myPlugin.js
+++ webpack-plugin/myPlugin.js
@@ -3,11 +3,42 @@ class MyPlugin {
 
     }
     apply(compiler) {
+        // compilation钩子---同步
         compiler.hooks.compilation.tap('HelloCompilationPlugin', (compilation) => {
             compilation.hooks.optimize.tap('HelloCompilationPlugin', () => {
-                console.log('Assets are being optimized.');
+                console.log('compilation钩子---同步');
             });
         });
+        //done钩子---同步
+        compiler.hooks.done.tap('Hello World Plugin', (
+            stats /* stats is passed as argument when done hook is tapped.  */
+        ) => {
+            console.log('done钩子---同步');
+        });
+        //emit钩子---异步
+        compiler.hooks.emit.tapAsync(
+            'MyExampleWebpackPlugin',
+            (compilation, callback) => {
+                console.log('emit钩子---异步');
+                callback();
+            }
+        );
+        //run钩子---异步
+        compiler.hooks.run.tapAsync(
+            'RunPlugin',
+            (compilation, callback) => {
+                console.log('run钩子---异步');
+                callback();
+            }
+        );
+        //make钩子---异步
+        compiler.hooks.run.tapAsync(
+            'MakePlugin',
+            (compilation, callback) => {
+                console.log('make钩子---异步');
+                callback();
+            }
+        );
     }
 }
 
