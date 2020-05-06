2693fa4c895823093aa4f40feccd29b047c12fce
ClimbYU <yumengkang@126.com>
2020-05-06 22:58:36 +0800

    编写自动插入dll插件

---
+++ plugin/DllScriptPlugin.js
@@ -0,0 +1,45 @@
+const { readdir } = require('fs')
+const { resolve } = require('path');
+const { cdn } = require('../env.js')
+const dllDir = resolve(__dirname, '../.temp/dll');
+
+function getJsFile() {
+  return new Promise((resolve) => {
+    readdir(dllDir, (error, files) => {
+      console.log(error);
+      if (!error) {
+        resolve(files)
+      } else {
+        resolve([])
+      }
+    })
+  })
+}
+
+class DllScriptPlugin {
+  apply(compiler) {
+    compiler.hooks.emit.tap('dllinsert', async compilation => {
+      const files = await getJsFile();
+      const { assets } = compilation;
+      files.map(file => {
+        if (/\.js$/.test(file)) {
+
+          for (const [key, value] of Object.entries(assets)) {
+            if (/\.html$/.test(key)) {
+              const assetValue = value.source();
+              value.source = () => {
+                return assetValue.replace(/<!--{{dll}}-->/g, `<script src="${cdn}/js/${file}"></script>`)
+              }
+
+              console.log(value.source())
+            }
+          }
+        }
+      })
+
+
+    })
+  }
+}
+
+module.exports = DllScriptPlugin
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const DllScriptPlugin = require('./plugin/DllScriptPlugin.js')
 const { cdn } = require('./env.js')
 const isDev = process.env.NODE_ENV === 'development' ? true : false
 
@@ -13,11 +14,11 @@ const dllplugin = function () {
     return new Promise((resolve) => {
         const plugin = [];
         const dllDir = path.resolve(__dirname, '.temp/dll');
-        path.readdir(dllDir, (error, files) => {
+        readdir(dllDir, (error, files) => {
             if (!error) {
                 files.map((file) => {
                     if (/\.json$/.test(file)) {
-                        plugin.push(new DllReferencePlugin({
+                        plugin.push(new webpack.DllReferencePlugin({
                             manifest: `${dllDir}/${file}`
                         }))
                     }
@@ -29,124 +30,124 @@ const dllplugin = function () {
     })
 }
 
-async function getDllPlugin() {
+async function getConfig() {
     const plugins = await dllplugin();
-    console.log(plugins)
-    return plugins
-}
+    const config = {
+        entry: './src/index',
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: '[name]_[chunkhash:8].js'
+        },
+        stats: {
+            assets: true,
+            modules: false,
+        },
+        // watch: true,
+        // watchOptions: {
 
-console.log(11111, getDllPlugin())
-
-module.exports = {
-    entry: './src/index',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name]_[chunkhash:8].js'
-    },
-    stats: {
-        assets: true,
-        modules: false,
-    },
-    // watch: true,
-    // watchOptions: {
-
-    // },
-    // mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.vue$/,
-                use: 'vue-loader',
-            },
-            {
-                test: /\.(css|less|scss)$/,
-                use: [
-                    {
-                        loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        options: isDev ? {} : {
-                            publicPath: cdn ? cdn : '../'
-                        }
-                    },// 必须放在上面用于解析字体或less
-                    { loader: 'css-loader' },
-                    {
-                        loader: 'postcss-loader',
-                    },
-                    {
-                        loader: 'less-loader'
-                    }, // less-loader需放在postloader后面
-                ]
-            },
-            {
-                test: /.(png|jpg|gif|jpeg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 1024 * 10
-                        }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4
-                            },
-                            optipng: {
-                                enabled: true
-                            },
-                            mozjpeg: {
-                                progressive: true,
-                                quality: [65, 95]
-                            },
-                            gifsicle: {
-                                interlaced: false
+        // },
+        // mode: 'production',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: 'babel-loader'
+                },
+                {
+                    test: /\.vue$/,
+                    use: 'vue-loader',
+                },
+                {
+                    test: /\.(css|less|scss)$/,
+                    use: [
+                        {
+                            loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                            options: isDev ? {} : {
+                                publicPath: cdn ? cdn : '../'
+                            }
+                        },// 必须放在上面用于解析字体或less
+                        { loader: 'css-loader' },
+                        {
+                            loader: 'postcss-loader',
+                        },
+                        {
+                            loader: 'less-loader'
+                        }, // less-loader需放在postloader后面
+                    ]
+                },
+                {
+                    test: /.(png|jpg|gif|jpeg)$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 1024 * 10
+                            }
+                        },
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                pngquant: {
+                                    quality: [0.65, 0.90],
+                                    speed: 4
+                                },
+                                optipng: {
+                                    enabled: true
+                                },
+                                mozjpeg: {
+                                    progressive: true,
+                                    quality: [65, 95]
+                                },
+                                gifsicle: {
+                                    interlaced: false
+                                }
                             }
                         }
-                    }
-                ]
-            },
-            {
-                test: /.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name]_[hash:8].[ext]'
+                    ]
+                },
+                {
+                    test: /.(woff|woff2|eot|ttf|otf)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name]_[hash:8].[ext]'
+                            }
                         }
-                    }
-                ],
-            }
+                    ],
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'src/index.html'),
+                filename: 'index.html',
+                // chunks: ['main'], // 可以指定使用哪些chunk
+                inject: true,
+                minify: {
+                    html5: true,
+                    collapseWhitespace: true,
+                    preserveLineBreaks: false,
+                    minifyCSS: true,
+                    minifyJS: true,
+                    removeComments: false
+                }
+            }),
+            new VueLoaderPlugin(), // vue加载需要此插件
+            new CleanWebpackPlugin(), // 删除dist
+            new MiniCssExtractPlugin({ // 提取css为单独文件
+                filename: '[name]_[contenthash:8].css'
+            }),
+            new OptimizeCssAssetsWebpackPlugin({ // 压缩css
+                assetNameRegExp: /\.css$/g,
+                // cssProcessor: require('cssnano') // 使用cssnano压缩
+            }),
+            new DllScriptPlugin(),
+            ...plugins,
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html'),
-            filename: 'index.html',
-            // chunks: ['main'], // 可以指定使用哪些chunk
-            inject: true,
-            minify: {
-                html5: true,
-                collapseWhitespace: true,
-                preserveLineBreaks: false,
-                minifyCSS: true,
-                minifyJS: true,
-                removeComments: false
-            }
-        }),
-        new VueLoaderPlugin(), // vue加载需要此插件
-        new CleanWebpackPlugin(), // 删除dist
-        new MiniCssExtractPlugin({ // 提取css为单独文件
-            filename: '[name]_[contenthash:8].css'
-        }),
-        new OptimizeCssAssetsWebpackPlugin({ // 压缩css
-            assetNameRegExp: /\.css$/g,
-            // cssProcessor: require('cssnano') // 使用cssnano压缩
-        }),
-        ...getDllPlugin()
-    ]
+    }
+
+    return config;
 }
+
+module.exports = getConfig()
