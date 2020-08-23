26bdd6534e67348921c7363db2d07a6d50c5e220
ClimbYU <yumengkang@126.com>
2020-08-23 17:32:24 +0800

    select组件封装

---
+++ src/index/components/index.js
@@ -0,0 +1,53 @@
+const env = process.env;
+export default {
+  data() {
+    return {
+      NODE_ENV: env.NODE_ENV,
+      startY: '',
+      endY: '',
+      scrollLocation: 0,
+      lastPosition: 0,
+      contentList: [2014, 2015, 2016,
+        2017, 2018, 2019,
+        2020, 2021, 2022, 2023, 2024, 2025,
+        2026, 2027, 2028
+      ],
+      selectedIndex: 0 //当前第几个被选中
+    };
+  },
+  mounted() {
+  },
+  methods: {
+    tap() {
+      console.log(1);
+    },
+    selectStart(e) {
+      const { touches = [] } = e;
+      const { clientY } = touches[0];
+      this.startY = clientY;
+      const selectContent = this.$refs['select_content'];
+      const translates = window.getComputedStyle(selectContent, null).transform
+      this.lastPosition = parseFloat(translates.substring(6).split(',')[5])
+    },
+    selectEnd(e) {
+      console.log()
+    },
+    selectMove(e) {
+      const { touches = [] } = e;
+      const { clientY } = touches[0];
+      this.endY = clientY;
+      const listRef = this.$refs['liList0'][0]
+      this.scrollLocation = this.lastPosition + this.endY - this.startY
+      const maxDistance = listRef.offsetHeight * (this.contentList.length - 1);
+      // 边界条件判断
+      if (this.scrollLocation > 0) {
+        this.scrollLocation = 0
+      } else if (Math.abs(this.scrollLocation) > maxDistance) {
+        this.scrollLocation = -maxDistance
+      }
+      // 获取当前滚动到第几个元素
+      this.selectedIndex = Math.round(Math.abs(this.scrollLocation / listRef.offsetHeight))
+    },
+
+  }
+};
---
+++ src/index/components/index.vue
@@ -0,0 +1,22 @@
+<style src="./index.less"></style>
+<script src="./index.js"></script>
+<template>
+  <div class="home">
+    <div class="container display-flex flex-col">
+      <div class="container-title display-flex flex-col-center flex-row-around">
+        <div>取消</div>
+        <div>请选择</div>
+        <div>确定</div>
+      </div>
+      {{selectedIndex}}
+      <div @touchstart="selectStart" @touchmove="selectMove" @touchend="selectEnd"
+        class="content-box display-flex flex-row-center">
+        <div class="content-box-top"></div>
+        <ul ref="select_content" class="select-content" :style="`transform:translate(0,${scrollLocation}px)`">
+          <li :ref="`liList${index}`" v-for="(item,index) in contentList" :key="index">{{item}}--{{index}}</li>
+        </ul>
+        <div class="content-box-bottom"></div>
+      </div>
+    </div>
+  </div>
+</template>
